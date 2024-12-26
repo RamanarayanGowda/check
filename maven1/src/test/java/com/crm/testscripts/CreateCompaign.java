9b0763ef7012405cd2d6e32d30fc4fdfa894fc6a
LENOVO <LENOVO@DESKTOP-P3JH12J>
2024-12-26 11:45:00 +0530

    1st commit

---
+++ maven1/src/test/java/com/crm/testscripts/CreateCompaign.java
@@ -0,0 +1,12 @@
+package com.crm.testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCompaign {
+	@Test
+	public void createCompain() {
+		Reporter.log("hello",true);
+	}
+
+}
